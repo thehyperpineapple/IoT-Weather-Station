d7b90b32903720ba090f8a0d6b3c80ce34e49387
TheLetifer <adityaverma.0412@gmail.com>
2024-03-29 00:52:03 +0530

    Finished ESP interace code

--- ESP32_Interfacing/firmware/main/main.ino
+++ ESP32_Interfacing/firmware/main/main.ino
@@ -1,76 +1,94 @@
 #include <Wire.h>
-#include <SPI.h>
 #include <Adafruit_BMP280.h>
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+#include <WiFiMulti.h>
 #include <DHT.h>
 
-#define BMP_SCK  (13)
-#define BMP_MISO (12)
-#define BMP_MOSI (11)
-#define BMP_CS   (10)
+// WiFi AP SSID
+#define WIFI_SSID "Wifi SSID"
+// WiFi password
+#define WIFI_PASSWORD "WiFi password"
+// InfluxDB v2 server url
+#define INFLUXDB_URL "Bucket URL"
+// InfluxDB v2 server or cloud API authentication token
+#define INFLUXDB_TOKEN "Bucket-Token"
+// InfluxDB v2 organization id
+#define INFLUXDB_ORG "Bucket ORG"
+// InfluxDB v2 bucket name
+#define INFLUXDB_BUCKET "Bucket Name"
 
-#define DHT_SENSOR_PIN  23
-#define DHT_SENSOR_TYPE DHT22
+#define TZ_INFO "IST-5:30" 
 
-Adafruit_BMP280 bmp; // BMP280 sensor
-DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE); // DHT22 sensor
+Adafruit_BMP280 bmp; // Declare the BMP280 object
+DHT dht_sensor(23, DHT22); // Declare the DHT22 object with pin 23
+
+WiFiMulti wifiMulti;
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
 
 void setup() {
-  Serial.begin(9600);
-  while (!Serial)
-    delay(100); // wait for native USB
+  Serial.begin(115200);
 
-  Serial.println(F("BMP280 and DHT22 Sensor Integration"));
+  // Setup wifi
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
 
-  // BMP280 setup
-  unsigned status = bmp.begin(0x76);
-  if (!status) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or try a different address!"));
-    while (1)
-      delay(10);
+  Serial.print("Connecting to wifi");
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
   }
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, Adafruit_BMP280::SAMPLING_X2,
-                  Adafruit_BMP280::SAMPLING_X16, Adafruit_BMP280::FILTER_X16,
-                  Adafruit_BMP280::STANDBY_MS_500);
-
-  // DHT22 setup
-  dht_sensor.begin();
-}
-
-void readBMP280() {
-  Serial.print(F("BMP280 - Pressure = "));
-  Serial.print(bmp.readPressure() / 100.0F);
-  Serial.println(" Pa");
-
-  Serial.print(F("BMP280 - Approx altitude = "));
-  Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-  Serial.println(" m");
-
-}
-
-void readDHT22() {
-  float humi = dht_sensor.readHumidity();
-  float tempC = dht_sensor.readTemperature();
-  float tempF = dht_sensor.readTemperature(true);
-
-  if (isnan(tempC) || isnan(tempF) || isnan(humi)) {
-    Serial.println("Failed to read from DHT sensor!");
-  } else {
-    Serial.print("DHT22 - Humidity: ");
-    Serial.print(humi);
-    Serial.println("%");
-
-    Serial.print("DHT22 - Temperature: ");
-    Serial.print(tempC);
-    Serial.print("°C  ~  ");
-    Serial.print(tempF);
-    Serial.println("°F");
-  }
-
   Serial.println();
+  
+  // Initialize BMP280 sensor
+  if (!bmp.begin(0x76)) {
+    Serial.println("Could not find a valid BMP280 sensor, check wiring!");
+    while (1);
+  }
+
+  // Initialize DHT sensor
+  dht_sensor.begin();
+
+  // Accurate time is necessary for certificate validation and writing in batches
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
 }
 
 void loop() {
-  readBMP280();
-  readDHT22();
-  delay(2000);
+  // Get latest sensor readings
+  float bmp_pressure = bmp.readPressure() / 100.0F; // Convert pressure to hPa
+  float bmp_altitude = bmp.readAltitude(1013.25);
+
+  // Read DHT22 sensor
+  float dht_temperature = dht_sensor.readTemperature();
+  float dht_humidity = dht_sensor.readHumidity();
+
+  // Create data points for BMP280 and DHT22
+  Point sensorReadings("bmp280 and dht22");
+  sensorReadings.addTag("location", "Lab");
+  sensorReadings.addField("altitude", bmp_altitude);
+  sensorReadings.addField("pressure", bmp_pressure);
+  sensorReadings.addField("temperature", dht_temperature);
+  sensorReadings.addField("humidity", dht_humidity);
+
+  
+  // Write the points into the buffer
+  client.writePoint(sensorReadings);
+
+  // If there is no Wifi signal, try to reconnect
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+
+  // Wait for 10 seconds
+  Serial.println("Waiting for 10 seconds");
+  delay(10000);
 }
